031c8bfdc4481d4c0080cb2bd8078aff70ba0e1f
Gabriel Campos <gcamposlive@gmail.com>
2026-01-13 19:40:36 -0300

    Project Spring, application.yaml, DataBaseCoonfiguration

---
+++ src/main/java/io/github/gcamposdev/libraryapi/config/DataBaseConfiguration.java
@@ -0,0 +1,55 @@
+package io.github.gcamposdev.libraryapi.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataBaseConfiguration {t
+
+    @Value("${spring.datasource.url}")
+    String url;
+    @Value("${spring.datasource.username}")
+    String username;
+    @Value("${spring.datasource.password}")
+    String password;
+    @Value("${spring.datasource.driverClassName}")
+    String driver;
+
+    /*
+    -> Outra forma de se conectar com o banco, uma forma mais simples e não muito usada
+    @Bean
+    public DataSource dataSoure(){
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setUrl(url);
+        ds.setUsername(username);
+        ds.setPassword(password);
+        ds.setDriverClassName(driver);
+        return ds;
+    }
+    */
+
+
+    @Bean
+    public DataSource hikariDataSource(){
+        HikariConfig config = new HikariConfig();
+
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
+        config.setDriverClassName(driver);
+
+        config.setMaximumPoolSize(10);
+        config.setMinimumIdle(1);
+        config.setPoolName("library-db-pool");
+        config.setMaxLifetime(600000);
+        config.setConnectionTimeout(100000);
+        config.setConnectionTestQuery("select 1");
+
+        return new HikariDataSource(config);
+    }
+}
